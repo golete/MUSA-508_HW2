735d70a808a7a58e0c2375cbbe3b034e063bdb5e
golete <golete@gmail.com>
2021-10-18 22:15:16 -0400

    Merged 01a_morewrangling file into the house variables, made buffers to the experimental variables.

--- 01_datawrangling.R
+++ 01_datawrangling.R
@@ -22,6 +22,12 @@ library(jtools)     # for regression model plots
 library(ggstance) # to support jtools plots
 library(mapview)
 
+# Working setup
+options(scipen = 999)
+g <- glimpse
+m <- mapview
+
+
 # --- DATA WRANGLING ---
 
 # A. HOME VALUE DATA
@@ -33,7 +39,6 @@ data <- st_read("studentData.geojson")
 st_crs(data$geometry)
 # ESPG 4326 in meters
 
-unique(data$'ConstCodeDscr')
 
 varsA <- c('price',
            'year_quarter',      # Time of sale
@@ -53,6 +58,23 @@ varsA <- c('price',
            'TotalFinishedSF',   # Total area (sq foot)
            'MUSA_ID',
            'geometry')
+
+
+# examine distribution of known prices
+knownPrice <- filter(data, toPredict == 0)
+
+# min(knownPrice$price) # $10,000
+# max(knownPrice$price) # $31,500,000
+# mean(knownPrice$price) # $749,112.9
+# mean(cleanerData$price) # $746,382.6
+# median(knownPrice$price) # 605,750
+# hist(cleanerData$price, breaks = 100)
+# hist(log(knownPrice$price), breaks = 100)
+# hist(log(cleanerData$price), breaks = 100)
+
+# hist(cleanerData$price)
+# hist(log(knownPrice$price))
+# hist(log(cleanerData$price))
 
 # designCodeDscr
 #   "1 Story - Ranch"
@@ -78,10 +100,153 @@ varsA <- c('price',
 #   "Veneer"
 #   "Precast" 
 
-house <- data %>%
-  dplyr::select(varsA) %>%
-  mutate(nbrRooms = nbrRoomsNobath+nbrFullBaths+(nbrThreeQtrBaths+nbrHalfBaths)/2)
-  
+houseData <- 
+  # exclude extreme outlier listed as sold for $31.5 million; 
+  # listing strongly suggests incorrectly entered: https://www.zillow.com/homedetails/3335-Talisman-Ct-APT-C-Boulder-CO-80301/13222117_zpid/
+  filter(data, price < 10000000) %>%
+  # create features from existing variables
+  mutate(
+    nbrRooms = nbrRoomsNobath+nbrFullBaths+(nbrThreeQtrBaths+nbrHalfBaths)/2,
+    # calculate quarter sold
+    quarterSold = str_sub(year_quarter, -1, -1),
+    # calculate log of price to normalize positive skew
+    logPrice = log(price),
+    # convert building and section numbers to dummies
+    manyBuildings = if_else(bld_num > 1, 1, 0),
+    manySections = if_else(section_num > 1, 1, 0),
+    # recode quality as numeric variable
+    # NOTE: not at all normal; maybe note in writeup if using
+    qualityNum = case_when(
+      qualityCode == 10 ~ 1, # QualityCodeDscr == "LOW "
+      qualityCode == 20 ~ 2, # "FAIR "
+      qualityCode == 30 ~ 3, # "AVERAGE "
+      qualityCode == 31 ~ 4, # "AVERAGE + "
+      qualityCode == 32 ~ 5, # "AVERAGE ++ "
+      qualityCode == 40 ~ 6, # "GOOD "
+      qualityCode == 41 ~ 7, # "GOOD + "
+      qualityCode == 42 ~ 8, # "GOOD ++ "
+      qualityCode == 50 ~ 9, # "VERY GOOD "
+      qualityCode == 51 ~ 10, # "VERY GOOD + "
+      qualityCode == 52 ~ 11, # "VERY GOOD ++ "
+      qualityCode == 60 ~ 12, # "EXCELLENT "
+      qualityCode == 61 ~ 13, # "EXCELLENT + "
+      qualityCode == 62 ~ 14, # "EXCELLENT++ "
+      qualityCode == 70 ~ 15, # "EXCEPTIONAL 1 "
+      qualityCode == 80 ~ 16, # "EXCEPTIONAL 2 "
+    ),
+    # try collapsing quality categories
+    # NOTE: still not at all normal, but see if more predictive
+    qualityNum2 = case_when(
+      qualityCode == 10 ~ 1, # "LOW"
+      qualityCode == 20 ~ 2, # "FAIR"
+      between(qualityCode, 30, 32) ~ 3, # "AVERAGE", "AVERAGE +", "AVERAGE ++"
+      between(qualityCode, 40, 42) ~ 4, # "GOOD", "GOOD +", "GOOD ++"
+      between(qualityCode, 50, 52) ~ 5, # "VERY GOOD", "VERY GOOD +", "VERY GOOD ++"
+      between(qualityCode, 60, 62) ~ 6, # "EXCELLENT", "EXCELLENT +", "EXCELLENT ++"
+      between(qualityCode, 70, 80) ~ 7 # "EXCEPTIONAL 1", "EXCEPTIONAL 2"
+    ),
+    # recode missing construction material values
+    constMat = case_when(
+      ConstCode == 0 ~ "Missing",
+      ConstCode == 300 ~ "Unspecified",
+      ConstCode > 300 ~ as.character(ConstCodeDscr)
+    ),
+    # calculate age and effective age (time since last major renovation)
+    # TODO: Decide whether to recode negative age (homes sold before construction) as 0
+    # TODO: Try effectiveAge with negatives recoded as 0, plus new construction dummy?
+    age = year - builtYear,
+    effectiveAge = year - EffectiveYear,
+    effectiveAge2 = if_else(year <= builtYear, 0, year - EffectiveYear), # negative values recoded as 0
+    newConstruction = if_else(year <= builtYear, 1, 0),
+    # recode missing basement values
+    basement = if_else(bsmtType == 0, "None", as.character(bsmtTypeDscr)),
+    # try simpler basement categories
+    basementDummy = if_else(bsmtType == 0, 0, 1),
+    finishedBasement = if_else(str_detect(bsmtTypeDscr, " FINISHED"), 1, 0),
+    walkOutBasement = if_else(str_detect(bsmtTypeDscr, "WALK"), 1, 0),
+    # recode missing car storage values
+    carStorage = if_else(carStorageType == 0, "None", as.character(carStorageTypeDscr)),
+    # try simpler car storage categories
+    carStorageSimpler = case_when(
+      carStorageType == 0 ~ "None",
+      str_detect(carStorageTypeDscr, "GARAGE") ~ "Garage",
+      str_detect(carStorageTypeDscr, "CARPORT") ~ "Carport",
+    ),
+    # try as dummies
+    carStorageDummy = if_else(carStorageType == 0, 0, 1),
+    garageDummy = if_else(str_detect(carStorageTypeDscr, "GARAGE"), 1, 0),
+    # calculate total bathrooms
+    totalBaths = nbrFullBaths + (0.75 * nbrThreeQtrBaths) + (0.5 * nbrHalfBaths),
+    totalBaths2 = nbrFullBaths + nbrThreeQtrBaths + (0.5 * nbrHalfBaths),
+    totalBathOrShower = nbrFullBaths + nbrThreeQtrBaths,
+    # recode missing a/c values
+    acType = case_when(
+      is.na(Ac) ~ "None",
+      Ac == 200 ~ "Unspecified", # Code with no description
+      Ac >= 210 ~ as.character(AcDscr) # "Attic Fan", "Evaporative Cooler", "Whole House" unchanged
+    ),
+    # create binary a/c feature to exclude fans, coolers, and unspecified
+    acDummy = replace_na(if_else(Ac == 210, 1, 0), 0),
+    # recode missing heating values
+    heatingType = case_when(
+      is.na(Heating) ~ "None",
+      Heating == 800 ~ "Unspecified",
+      Heating > 800 ~ as.character(HeatingDscr)
+    ),
+    # recode missing primary exterior wall values
+    extWall = if_else(ExtWallPrim == 0, "Missing", as.character(ExtWallDscrPrim)),
+    # recode missing secondary exterior wall values
+    extWall2 = if_else(is.na(ExtWallSec), "None", as.character(ExtWallDscrSec)),
+    # recode missing interior wall values
+    intWall = if_else(is.na(IntWall), "Missing", as.character(IntWallDscr)),
+    # recode missing roof cover values
+    roofType = if_else(is.na(Roof_Cover), "Missing", as.character(Roof_CoverDscr))
+  )
+
+
+house <- houseData %>%
+  # drop unnecessary columns and columns with too much missing data
+  dplyr::select(
+    # same for all
+    -bldgClass,
+    -bldgClassDscr,
+    -status_cd,
+    # not needed
+    -saleDate,
+    -address,
+    -designCode, # see designCodeDscr
+    # too much missing data
+    -Stories,
+    -UnitCount,
+    # recoded
+    # -year_quarter, # compare with year and quarter as separate features?
+    -bld_num,
+    -section_num,
+    -qualityCode,
+    -qualityCodeDscr,
+    -ConstCode,
+    -ConstCodeDscr,
+    -builtYear,
+    -EffectiveYear,
+    -bsmtType,
+    -bsmtTypeDscr,
+    -carStorageType,
+    -carStorageTypeDscr,
+    -Ac,
+    -AcDscr,
+    -Heating,
+    -HeatingDscr,
+    -ExtWallPrim,
+    -ExtWallDscrPrim,
+    -ExtWallSec,
+    -ExtWallDscrSec,
+    -IntWall,
+    -IntWallDscr,
+    -Roof_Cover,
+    -Roof_CoverDscr
+  )
+
+
 hist(house$nbrRooms)
 
 
@@ -90,12 +255,10 @@ hist(house$nbrRooms)
 
 # B1. Boulder county boundaries
 
-
 countylimits <- st_read('County_Boundary.geojson') %>%
   select(OBJECTID, geometry)
 st_crs(countylimits)
 # CRS: EPSG: 4326, WGS84, metres
-
 
 munis <- st_read('Municipalities.geojson') %>%
   select(ZONEDESC, geometry)
@@ -103,8 +266,10 @@ st_crs(munis)
 # CRS: EPSG: 4326, WGS84, metres
 
 
-
 # B2. Boulder city and other cities/zones boundaries
+
+# To solve problem with spherical, ask for implications ****
+sf::sf_use_s2(FALSE)
 
 zones <- st_read('Zoning_-_Zoning_Districts.geojson') %>%
   select(ZONEDESC, geometry) %>%
@@ -115,6 +280,7 @@ zones <- st_read('Zoning_-_Zoning_Districts.geojson') %>%
 st_crs(zones)
 # CRS: EPSG: 4326, WGS84, metres
 
+# Union the polygons that are not in boulder City limits, just in case
 notCity <- zones %>% st_union
 mapview(notCity)
 
@@ -125,15 +291,10 @@ districts <- st_read('Zoning_Districts.geojson') %>%
 st_crs(districts)
 # CRS: EPSG: 4326, WGS84, metres
 
-
 # Load the subcommunities / neighborhoods rough boundaries
 subcomms <-  st_read('Subcommunities.geojson')
 st_crs(subcomms)
 # CRS: EPSG: 4326, WGS84, metres
-
-
-# Problem with spherical, SOLVE
-sf::sf_use_s2(FALSE)
 
 # Join the region zoning polygons with the subcommunities polygons and union
 cityHoods <- st_join(districts, subcomms, largest=TRUE) %>%
@@ -141,11 +302,28 @@ cityHoods <- st_join(districts, subcomms, largest=TRUE) %>%
   group_by(SUBCOMMUNITY) %>%
   summarize(geometry = st_union(geometry))
 
+mapview(zones)
+mapview(cityHoods)
 
 neighborhoods <- rbind(zones, cityHoods)
-mapview(neighborhoods)
+m(neighborhoods)
 
-
+# Subset the generic zones as a separate map just if needed
+genericZones <- c('Business',
+                  'Commercial',
+                  'Economic Development',
+                  'Estate Residential',
+                  'General Industrial',
+                  'Light Industrial',
+                  'Manufactured Home',
+                  'Mountain Institutional',
+                  'Multiple Family',
+                  'Rural Residential',
+                  'Suburban Residential',
+                  'Transitional')
+uZones <- zones %>%
+  filter(SUBCOMMUNITY %in% genericZones)
+m(uZones)
 # COMPARE WITH NOT-CITY boundaries
 
 
@@ -155,7 +333,6 @@ mapview(neighborhoods)
 # set API key
 # NOTE: Set overwrite to "false" to prevent overwriting my own key locally -EE
 census_api_key("e79f3706b6d61249968c6ce88794f6f556e5bf3d", overwrite = FALSE)
-
 
 year <- 2019
 state <- 08
@@ -222,7 +399,6 @@ st_crs(tracts$geometry) # CRS: ESPG 4269, NAD84 metres
 
 
 
-
 # D. OTHER DATA (CRIME, FEMA, etc.)
 
 # D1. Wildfire history data
@@ -270,24 +446,33 @@ st_crs(floodplains) # CRS: EPSG 4326, WGS 84, metres
 mapview(floodplains$geometry)
 
 
-# D3. FEMA flood insurance map
-
-
-
-# TODO: Figure out others
-
-
 
 # E. EXPERIMENTAL DATA
 
 # E1. Whole Foods locations
-wholefoods <- st_read("wholefoodsmarkets_boulderCO.csv") %>%
-  dplyr::select(-phone) %>%
-  st_as_sf(coords= c('lat','lon'), crs = 4269)
 
-# TODO: Assessor mill levy (property tax rate)
-# TODO: Airport influence?
-# TODO: Figure out others
+wholefoodsData <- st_read("wholefoodsmarkets_boulderCO.csv")
+
+wholefoods <- st_as_sf(wholefoodsData, coords = c("lon", "lat"), crs = 4326) %>%
+  st_transform(crs = 3857) %>%
+  dplyr::select(-phone, -address) %>%
+  st_buffer(5000) %>%
+  st_union()
+
+m(wholefoods)
+
+# E2. Marijuana dispensaries
+marijuana <- st_read("Marijuana_Establishments.geojson") %>%
+  st_transform(crs = 3857) %>%
+  dplyr::select(OBJECTID, Type, geometry) %>%
+  st_buffer(2414) %>%
+  st_union()
+
+m(marijuana)
+
+st_crs(marijuana) # CRS: EPSG:4326, WGS 84
+
+
 
 # --- EXPORT ---
 
